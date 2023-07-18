ba651b52347497e8ebd5df5f46a15f31bb5fa097
“Zuzana <zuzana@getyourguide.com>
2023-07-18 21:31:45 +0200

    Added background colors

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,15 +2,19 @@ function Layout({ children }) {
     return (
       <div>
         <nav>
-          <div className="app-title"> Too Lazy to Type</div>
-          <ul className="nav-links" >
-            <li><a className="nav-link" href="/login">Login</a></li>
-            <li><a className="nav-link" href="/signup">Register</a></li>
-            <li><a className="nav-link" href="/">About us</a></li>
-            <li><a className="nav-link" href="/">Contact us</a></li>
-          </ul>
-          </nav>
-          <main>{children}</main>    
+            <div className="app-title"> Too Lazy to Type</div>
+            <ul className="nav-links" >
+              <li><a className="nav-link" href="/login">Login</a></li>
+              <li><a className="nav-link" href="/signup">Register</a></li>
+              <li><a className="nav-link" href="/">About us</a></li>
+              <li><a className="nav-link" href="/">Contact us</a></li>
+            </ul>
+            </nav>
+          <body>
+            <main>
+              {children}
+            </main>
+          </body>    
 
       </div>
     )
